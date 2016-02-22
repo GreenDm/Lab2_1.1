7f59bab7bb570ac5a3427c167bb3abb43fc6f38d
HrinchenkoDmytro <d.greenchenko@gmail.com>
2016-02-22 11:58:03 +0200

    Initial commit

---
+++ src/epam/com/Main.java
@@ -0,0 +1,14 @@
+package epam.com;
+
+/**
+ * Created by Любовь on 22.02.2016.
+ */
+public class Main {
+
+    private String ManuFacture;
+    private int serialNumber;
+    private float price;
+    private int quantityCPU;
+    private int frequencyCPU;
+
+}
